eefd46bd408510fc2615eeac99d72947376717c9
hyy369 <hyy369@users.noreply.github.com>
2018-04-02 22:35:23 -0400

    add more sprites

--- Stalk.py
+++ Stalk.py
@@ -17,11 +17,8 @@ class Stalk:
         self.length = length
 
     def grow(self):
-        rand_length = random.randint(100, 300)
-        # rand_length = 200
-        rand_angle = random.randint(-5, 5)
-        # rand_angle = 5
-        # print("top", self.top.x, self.top.y)
+        rand_length = get_segment_length()
+        rand_angle = get_random_angle()
         new_seg = Segment(self.top.position, self.direction.rotate(rand_angle), rand_length)
         self.top = Joint(new_seg.get_end().x, new_seg.get_end().y, new_seg)
         if self.decide_grow_branch():
@@ -37,14 +34,13 @@ class Stalk:
                 self.top.r_branch = new_branch
             self.branches.append(new_branch)
         self.joints.append(self.top)
-        # print("new_top",self.top.x,self.top.y)
         self.segments.append(new_seg)
         self.count = len(self.segments)
 
     def decide_grow_branch(self):
         if len(self.segments) + 1 == self.length:
             return False
-        i = random.randint(1,10)
+        i = random.randint(1, 10)
         if i <= 4:
             return True
         else:
@@ -52,4 +48,12 @@ class Stalk:
 
 
 def generate_segment_count():
-    return random.randint(1, 2)
+    return random.randint(3,5)
+
+
+def get_segment_length():
+    return random.randint(242, 335)
+
+
+def get_random_angle():
+    return random.randint(-5, 5)
--- main.py
+++ main.py
@@ -9,42 +9,27 @@ from Vec2d import Vec2d
 from Stalk import Stalk
 
 
-def generate_bamboo_quantity():
-    return random.randint(2, 4)
-    # return 1
-
-
-def generate_segment_count():
-    return random.randint(3, 5)
-    # return 3
-
-
-def generate_root_position(width):
-    return Vec2d(random.randint(int(0.33 * width), int(0.5 * width)), 0)
-    # return Vec2d(256, 0)
-
-
-def generate_new_root(root):
-    return Vec2d(root + random.randint(30, 60), 0)
-
-
 def main():
+    # Initializations
     timestamp = str(int(time.time()))
     filename1 = "./sample_output/" + timestamp + "_sturcture.png"
     filename2 = "./sample_output/" + timestamp + "_render.png"
-    size = 512
-    canvas_struct = Canvas(size, size)
-    canvas_render = Canvas(size, size)
+    width = 480
+    height = 720
+    canvas_struct = Canvas(width, height)
+    canvas_render = Canvas(width, height)
     png_painter = Painter()
 
-    stalk_sprite = imageio.imread('./sprite/stalk-1.png')
-    branch_sprite = imageio.imread('./sprite/branch-1.png')
-    leaf_sprite = imageio.imread('./sprite/leaf-1.png')
+    # Read sprites
+    stalk_sprite = [imageio.imread('./sprite/stalk-1.png'), imageio.imread('./sprite/stalk-2.png'), imageio.imread('./sprite/stalk-3.png')]
+    branch_sprite = [imageio.imread('./sprite/branch-1.png'), imageio.imread('./sprite/branch-2.png')]
+    leaf_sprite = [imageio.imread('./sprite/leaf-1.png'), imageio.imread('./sprite/leaf-2.png'), imageio.imread('./sprite/leaf-3.png'), imageio.imread('./sprite/leaf-4.png'), imageio.imread('./sprite/leaf-5.png')]
+    seal_sprite = imageio.imread('./sprite/seal-90.png')
 
     print("Constructing...")
 
     quantity = generate_bamboo_quantity()
-    newest_root = generate_root_position(size)
+    newest_root = generate_root_position(width)
     roots = list()
     roots.append(newest_root)
     for i in range(quantity - 1):
@@ -59,30 +44,71 @@ def main():
             new_stalk.grow()
         stalks.append(new_stalk)
 
+    stalk_count = 1
+    stalk_total = len(stalks)
     for stalk in stalks:
-        print("Rendering stalks...")
+        print("Rendering stalk", stalk_count, "/", stalk_total)
+        shade = get_shade_degree()
         for seg in stalk.segments:
             # print(seg.origin.x, seg.origin.y)
             # print(seg.direction.x, seg.direction.y)
             # print(seg.get_end().x, seg.get_end().y)
             canvas_struct.paint_seg_black(seg)
-            canvas_render.paint_seg_sprite(seg, stalk_sprite)
+            canvas_render.paint_seg_sprite(seg, stalk_sprite[get_stalk_sprite_index()], shade)
 
         for branch in stalk.branches:
-            for seg in branch.segments:
-                canvas_struct.paint_seg_blue(seg)
-                canvas_render.paint_seg_sprite(seg, branch_sprite)
+            seg = branch.get_segment()
+            canvas_struct.paint_seg_blue(seg)
+            canvas_render.paint_seg_sprite(seg, branch_sprite[get_branch_sprite_index()], shade)
             for joint in branch.joints:
                 canvas_struct.paint_joint_blue(joint)
             for leaf in branch.leaves:
                 canvas_struct.paint_seg_green(leaf)
-                canvas_render.paint_seg_sprite(leaf, leaf_sprite)
+                canvas_render.paint_seg_sprite(leaf, leaf_sprite[get_leaf_sprite_index()], shade)
         for joint in stalk.joints:
             canvas_struct.paint_joint_red(joint)
+        stalk_count += 1
+
+    canvas_render.paint_seal(seal_sprite)
 
     print("Saving images...")
     png_painter.paint_png(canvas_struct, filename1)
     png_painter.paint_png(canvas_render, filename2)
+
+
+def generate_bamboo_quantity():
+    # return random.randint(2, 4)
+    return 1
+
+
+def generate_segment_count():
+    return random.randint(3, 4)
+    # return 3
+
+
+def generate_root_position(width):
+    return Vec2d(random.randint(int(0.33 * width), int(0.5 * width)), 0)
+    # return Vec2d(256, 0)
+
+
+def generate_new_root(root):
+    return Vec2d(root + random.randint(30, 60), 0)
+
+
+def get_stalk_sprite_index():
+    return random.randint(0, 2)
+
+
+def get_branch_sprite_index():
+    return random.randint(0, 1)
+
+
+def get_leaf_sprite_index():
+    return random.randint(0, 4)
+
+
+def get_shade_degree():
+    return random.randint(7, 10) / 10
 
 
 main()
